6cf5e06a3418d1516f3de7cf9dc6ea7932983b10
Jasin Mustafi <121194092+Jay-M1@users.noreply.github.com>
2024-02-05 21:15:50 +0100

    drehendes rect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import numpy as np
+import math
 from pathlib import Path
 from numpy.random import randint
 from copy import copy
@@ -28,6 +29,45 @@ def main():
        
     def GameOver():
         reset()
+
+    def draw_rectangle(x, y, width, height, color, rotation=0):
+        """Draw a rectangle, centered at x, y.
+
+        Arguments:
+        x (int/float):
+            The x coordinate of the center of the shape.
+        y (int/float):
+            The y coordinate of the center of the shape.
+        width (int/float):
+            The width of the rectangle.
+        height (int/float):
+            The height of the rectangle.
+        color (str):
+            Name of the fill color, in HTML format.
+        """
+        points = []
+
+        # The distance from the center of the rectangle to
+        # one of the corners is the same for each corner.
+        radius = math.sqrt((height / 2)**2 + (width / 2)**2)
+
+        # Get the angle to one of the corners with respect
+        # to the x-axis.
+        angle = math.atan2(height / 2, width / 2)
+
+        # Transform that angle to reach each corner of the rectangle.
+        angles = [angle, -angle + math.pi, angle + math.pi, -angle]
+
+        # Convert rotation from degrees to radians.
+        rot_radians = (math.pi / 180) * rotation
+
+        # Calculate the coordinates of each point.
+        for angle in angles:
+            y_offset = -1 * radius * math.sin(angle + rot_radians)
+            x_offset = radius * math.cos(angle + rot_radians)
+            points.append((x + x_offset, y + y_offset))
+
+        pygame.draw.polygon(screen, color, points)
     
     def paint_line(start, end, tast=9, ballhere = True):
             """
@@ -131,6 +171,8 @@ def main():
     key_right = False
     
     rect_speed = 2
+
+    i = 1
     
     # Main event loop
     while running:
@@ -205,8 +247,9 @@ def main():
         hole2_rect = hole2_surface.get_rect(bottomright = (screen.get_width(),screen.get_height()))
         screen.blit(hole1_surface,hole1_rect)
         screen.blit(hole2_surface,hole2_rect)
-        
-        
+
+        i += 2
+        draw_rectangle(200,200,50,50,colors['black'],i)
     
         
         # Motion
